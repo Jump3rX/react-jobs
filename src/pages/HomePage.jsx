336edfba46968b259c46a3d16bd7caf9efd196fc
Conrad <conradmax5@gmail.com>
2024-04-26 17:38:14 +0300

    practice code

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,16 @@
+import Hero from "../components/Hero";
+import HomeCards from "../components/HomeCards";
+import Listings from "../components/Listings";
+import ViewAllJobs from "../components/ViewAllJobs";
+const HomePage = () => {
+  return (
+    <>
+      <Hero />
+      <HomeCards />
+      <Listings isHome={true} />
+      <ViewAllJobs />
+    </>
+  );
+};
+
+export default HomePage;
---
+++ src/pages/JobsPage.jsx
@@ -0,0 +1,10 @@
+import Listings from "../components/Listings";
+function JobsPage() {
+  return (
+    <section className="bg-blue-50 px-4 py-6">
+      <Listings />
+    </section>
+  );
+}
+
+export default JobsPage;
